7a5e9990e22a3456d4642bfa1b6e013b2ba6f896
Sara JC Gosline <sara.gosline@sagebase.org>
2019-04-06 16:44:00 -0700

    updated

--- analysis/2019-04-06/runMetaviperOnTcga.R
+++ analysis/2019-04-06/runMetaviperOnTcga.R
@@ -13,18 +13,21 @@ net.names <- data(package="aracne.networks")$results[, "Item"]
 all.networks <- lapply(net.names,function(x) get(x))
 names(all.networks) <- net.names
 
+entrez.genes<-sapply(as.character(all.tcga$gene_id),function(x) unlist(strsplit(x,split='|',fixed=T))[2])
+rownames(all.tcga)<-entrez.genes
+all.tcga<-all.tcga[,2:ncol(all.tcga)]
 #first add in entrez
 require(biomaRt)
 mart <- useMart('ensembl',dataset='hsapiens_gene_ensembl')
-
-entrez_list <- getBM(filters = "hgnc_symbol", 
+#library(tidyverse)
+entrez_list <- getBM(filters ="entrezgene", 
   attributes = c("hgnc_symbol", "entrezgene"), 
-  values = unique(expData$Symbol), mart = mart)%>%
-  rename(Symbol='hgnc_symbol')%>%
-  right_join(expData,by='Symbol')%>%tidyr::unite('entre_syn',entrezgene,id)
+  values =entrez.genes, mart = mart)
+#  rename(Symbol='hgnc_symbol')%>%
+#  right_join(expData,by='Symbol')%>%tidyr::unite('entre_syn',entrezgene,id)
 
-combined.mat=reshape2::acast(entrez_list,entrezgene~id,value.var="zScore",fun.aggregate=function(x) mean(x,na.rm=T))
-res <- viper(combined.mat, all.networks)
+#combined.mat=reshape2::acast(entrez_list,entrezgene~id,value.var="zScore",fun.aggregate=function(x) mean(x,na.rm=T))
+res <- viper(all.tcga, all.networks)
 
 ##now re-shape to be tidy again and store!
 rr<-tidyr::gather(data.frame(res,entrezgene=rownames(res)),key=id,value=viper,-entrezgene)%>%tidyr::unite('entre_syn',entrezgene,id)
@@ -35,5 +38,5 @@ vip.res<-rr%>%
   separate(entre_syn,c('entrez','id'))
 
 
-tab<-synBuildTable(name=paste(lubridate::today(),'metaVIPER proteins'),vip.res,parent='syn16941818')
+tab<-synBuildTable(name=paste(lubridate::today(),'TCGA metaVIPER proteins'),vip.res,parent='syn16941818')
 synStore(tab)
