670df5597a0edbf4ed84a4ce5d4490f39cb796c8
Robert Allaway <allaway@users.noreply.github.com>
2019-04-10 15:06:07 -0700

    add code to plot individual dr curves

--- bin/plotDrugsAcrossCells.R
+++ bin/plotDrugsAcrossCells.R
@@ -26,6 +26,81 @@ plotDrugByCellAndTumor<-function(compoundName,tumorType){
       ggsave(fname)
     }}
   return(fname)
- # ggplot(red.tab)+geom_violin(aes(x=symptom_name,y=response,col=organism_name))+facet_grid(.~response_type)+ggtitle(paste(compoundName,'response across cells'))+theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  # ggplot(red.tab)+geom_violin(aes(x=symptom_name,y=response,col=organism_name))+facet_grid(.~response_type)+ggtitle(paste(compoundName,'response across cells'))+theme(axis.text.x = element_text(angle = 45, hjust = 1))
   
 }
+
+screening_data_syn_id <- "syn17100888"
+
+screening_data_with_id <- synGet(screening_data_syn_id)$path %>% 
+  readr::read_csv(guess_max=100000) %>% 
+  dplyr::rename(internal_id='DT_explorer_internal_id') %>% 
+  dplyr::left_join(drug.map)
+
+nplr_dosage_response <- function(df){
+  foo <- tryCatch({nplr::nplr(df$dosage, df$response)},error = function(e) {
+    return(NA)
+  })
+}
+
+get_ic50 <- function(mod){
+  nplr::getEstimates(mod)[5,3]
+}
+
+plotDoseResponseCurve<-function(compoundName,tumorTypes,scaleY = F, minLogDose = -6, minDosesPerGroup = 5){
+
+    red.tab <- subset(screening_data_with_id,std_name==compoundName) %>% 
+      dplyr::group_by(drug_screen_id) %>% 
+      dplyr::filter(n() >= minDosesPerGroup) %>% #eliminate small dose-response groups with few points
+      dplyr::filter(min(log10(dosage)) >= minLogDose) %>% 
+      dplyr::ungroup() %>% 
+      dplyr::filter(symptom_name %in% tumorTypes) %>% 
+      tidyr::nest(-drug_screen_id, -symptom_name, -drug_name) %>% 
+      dplyr::mutate(model = purrr::map(data, nplr_dosage_response)) %>% ##4 or 5 param logistic regression of curve
+      dplyr::mutate(ic50 = purrr::map(model, get_ic50) )%>% 
+      dplyr::mutate(x = purrr::map(model, nplr::getXcurve)) %>% ##extract x and y fitted curve values
+      dplyr::mutate(y = purrr::map(model, nplr::getYcurve)) %>% 
+      dplyr::mutate(y_scale = purrr::map(y, scale)) %>% ##scale y to clean up across d-rs 
+      dplyr::select(-data, -model) %>% 
+      tidyr::unnest(.preserve=ic50) %>% 
+      dplyr::group_by(drug_screen_id) %>% 
+      dplyr::mutate(mean_ic50 = signif(mean(unlist(ic50), na.rm = T),3)) %>% 
+      ungroup()
+    
+  
+    fname <- NULL
+  
+  if(missing(tumorTypes)||tumorTypes%in%red.tab$symptom_name){
+    if(nrow(red.tab)>0){
+      if(scaleY == F){
+      ggplot(red.tab)+
+        geom_path(aes(x=x,y=y,col=symptom_name, group=as.factor(drug_screen_id)))+
+          labs(title = paste0(compoundName, " mean IC50: ", red.tab$mean_ic50, " uM"),
+               x = expression("log10(["*mu*"M])"), 
+               y = "% viability",
+               col = "Tumor Type")  +
+          theme_bw() +
+          theme(axis.text = element_text(size = 12),
+                axis.title = element_text(size = 15))
+      }else{
+        ggplot(red.tab)+
+          geom_path(aes(x=x,y=y_scale,col=symptom_name, group=as.factor(drug_screen_id)))+
+          labs(title = paste0(compoundName, " mean IC50: ", red.tab$mean_ic50, " uM"),
+               x = expression("log10(["*mu*"M])"), 
+               y = "scaled % viability",
+               col = "Tumor Type") +
+          theme_bw() +
+          theme(axis.text = element_text(size = 12),
+                axis.title = element_text(size = 15))
+      }
+      fname=paste0('drugResponsesFrom',compoundName,'with',paste0(tumorTypes, collapse = "_"),'.png')
+      ggsave(fname)
+    }
+    }
+  
+  return(fname)
+}
+
+##examples:
+#plotDoseResponseCurve("ENMD-2076", c("pNF"), scaleY = F, minDosesPerGroup = 2, minLogDose = -6)
+#plotDoseResponseCurve("PF-3758309", c("pNF", "MPNST"), scaleY = F, minDosesPerGroup = 2, minLogDose = -6)
