75ed61a68f76dd208f93e217ee16b7bf463aaa81
sgosline <sara.gosline@sagebase.org>
2019-05-06 21:19:36 +0000

    udpated

--- bin/copyNumberAnalysis.R
+++ bin/copyNumberAnalysis.R
@@ -8,7 +8,7 @@ runCnvAnalysis<-function(bam.file.list,ncores){
    target.file<-synGet('syn18078824')$path
    reference<-synGet('syn18082228')$path
 
-   bam.files<-mclapply(bam.file.list,function(x) synGet(x)$path,mc.cores=ncores)
+   bam.files<-lapply(bam.file.list,function(x) synGet(x)$path)#,mc.cores=ncores)
    sample.names<-names(bam.file.list)
    target.df <- read.delim(target.file, header = FALSE)
    target <- GRanges(seqname = target.df[, 1], IRanges(start = target.df[, 2] + 1, end = target.df[, 3]))
@@ -42,14 +42,17 @@ counts$bg <- generateBackground(sample.names, counts, median)
 
 plotSegs<-function(compiled.segments){
    library("RColorBrewer")
-   cols=brewer.pal(n = 8, name = "RdBu")
+   
    abnorm=compiled.segments[compiled.segments$copy.count != 2]
+   cols=brewer.pal(n = length(unique(abnorm$copy.count)), name = "Spectral")
    seqs=unique(compiled.segments@seqnames)
    file.list<-c()
    for(s in seqs){
       fname=paste('chrom',s,'copyNumb.png',sep='')
       png(fname)
-      plotCompiledCNV(abnorm,seq.name=s,col=cols)
+      #chr.start <- start(range(abnorm@ranges))
+      #chr.end <- end(range(abnorm@ranges))
+      plotCompiledCNV(abnorm,copy.counts= unique(abnorm$copy.count),seq.name=s,col=cols)#,xlim=c(chr.start,chr.end))
       dev.off()
       file.list<-c(file.list,fname)
    }
